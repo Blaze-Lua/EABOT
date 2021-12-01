b8b0ff933a71cc8af6b884d02029c386eea9364e
Koharu <blazeannex@gmail.com>
2021-12-01 20:27:10 +0530

    verion 1.1

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import discord
-from discord.ui import Select,Button,View
-from options import Options
+from discord.ui import Select, View
+
+#all Select stuff
+option1 = discord.SelectOption(label='Youtube', description="Watch youtube with your gang!"),
+option2 = discord.SelectOption(label='Chess', description="Play chess with your gang!"),
+option3 = discord.SelectOption(label='Poker', description="Play poker with your gang!")
+option4 = discord.SelectOption(label='Betrayal', description="Play betrayal with your gang!")
+option5 = discord.SelectOption(label='Fishing', description="Play Fishing with your gang!")
+option6 = discord.SelectOption(label='Letter-tile', description="Play Letter-tile with your gang!")
+option7 = discord.SelectOption(label='Word-snack', description="Watch Word-snack with your gang!")
+option8 = discord.SelectOption(label='Doodle-crew', description="Play Doodle-crew with your gang!")
+option9 = discord.SelectOption(label='Spellcast', description="Play spellcast with your gang!")
+option10 = discord.SelectOption(label='Checkers-in-the-park', description="Play checkers with your gang!")
+option11 = discord.SelectOption(label='Awkword', description="Play awkword with your gang!")
 
 Bot = discord.Bot()
 
@@ -13,7 +25,7 @@ async def watch(
         ctx,
 ):
     #Making the select Menu
-    menu = Select(placeholder="You find yourself in a confusing place." , options=[Options.option1 , Options.option2,Options.option3,Options.option4,Options.option4,Options.option5,Options.option6,Options.option7,Options.option8,Options.option9,Options.option10,Options.option11])
+    menu = Select(placeholder="You find yourself in a confusing place.",min_values=15,options=[option1 ,option2,option3,option4,option5,option6,option7,option8,option9,option10,option11])
 
     view = View()
     view.add_item(menu)
